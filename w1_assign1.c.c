f588d6b73c2ab59b72a0d33488715898d4487dde
Sibasisbadatya <98338367+Sibasisbadatya@users.noreply.github.com>
2022-02-13 11:45:23 +0530

    Update w1_assign1.c.c

--- w1_assign1.c.c
+++ w1_assign1.c.c
@@ -1,6 +1,6 @@
 #include<stdio.h> 
 
 int main(){
-    printf("Name:Prem Prabhujeet\nRegistation number:010\nBranch:Computer Science and Engineering\nHobbies: Playing cricket and Running");
+    printf("Name:sibasis badatya\nRegistation number:046\nBranch:Computer Science and Engineering\nHobbies: Playing cricket");
     return 0;
 }
